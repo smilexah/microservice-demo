548d233133ad110575dd978d994fab2ae174aee6
smilexah <smilexah@inbox.ru>
2024-08-22 06:15:08 +0500

    config api gateway and distribute the configuration

--- config-server/src/main/java/sdu/edu/kz/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/sdu/edu/kz/configserver/ConfigServerApplication.java
@@ -2,7 +2,9 @@ package sdu.edu.kz.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApplication {
 
